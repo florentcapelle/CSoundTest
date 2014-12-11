2c833cc18261611184781534a0abc9b850f2e43d
Florent Capelle <florentcapelle@Florents-MacBook-Pro.local>
2014-12-12 00:04:28 +0100

    Initial Commit

---
+++ CSoundTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSoundTest
+//
+//  Created by Florent Capelle on 12/12/14.
+//  Copyright (c) 2014 Florent Capelle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
