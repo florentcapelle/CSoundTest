91545364416914c0b12b15b4ad904797b9d460ed
florentcapelle <florent.capelle.fc@gmail.com>
2015-04-14 22:00:38 +0200

    test one two

--- CSoundTest/main.c
+++ CSoundTest/main.c
@@ -1,15 +1,37 @@
-//
-//  main.c
-//  CSoundTest
-//
-//  Created by Florent Capelle on 12/12/14.
-//  Copyright (c) 2014 Florent Capelle. All rights reserved.
-//
+#include "stdio.h"
+#include "stdlib.h"
+#include "csound.h"
+#include "cscore.h"
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+static void eval(x)
+{
+    if (x != CSOUND_SUCCESS)
+    {
+        printf("!! Function error %d !!", x);
+    }
+}
+
+
+int main()
+{
+    void* csound;
+ 
+    // Create Csound.
+    csound = csoundCreate(0);
+    
+    // Start it
+
+//    eval(csoundSetOption(csound, "-odac0"));
+    eval(csoundCompileCsd(csound, "/Users/florentcapelle/Documents/CSOUND/CSOUND Test/test.csd"));
+    eval(csoundPerform(csound));
+
+
+        
+        
+        
+    csoundCleanup(csound);
+    csoundDestroy(csound);
+    
     return 0;
 }
